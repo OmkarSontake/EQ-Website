107506fae161b4babfc6816483824fe508593eb4
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2025-03-25 15:04:38 +0530

    upodate

--- components/icon-cta.vue
+++ components/icon-cta.vue
@@ -1,10 +1,41 @@
 <template>
-    <div class="w-full bg-white text-white background-image-cta-icon">
-        <div class="mx-auto max-w-[85rem] px-4 sm:px-6 lg:px-8 py-5 sm:py-8">
+    <div class="w-full bg-white text-white background-image-cta-icon relative">
+        <img src="/registration-pg-picture.jpg" class="h-full w-full absolute object-cover" alt="">
+        <div class="mx-auto max-w-[85rem] px-4 sm:px-6 lg:px-8 py-5 sm:py-8 opacity-0">
             <div class="py-6"></div>
             <h2 class="text-center w-full text-3xl lg:text-4xl 2xl:text-5xl font-semibold">IMPROVE YOUR MARKET INTELLIGENCE <br class="hidden lg:block"> WITH 
                 EQ ANALYSIS</h2>
-
+            <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-16">
+                <div class="flex flex-col items-center text-center">
+                    <img src="https://astro-agency08.vercel.app/assets/img/icons/icon-tangerine-feature-1.svg"
+                        class="h-18 hue-rotate-60 invert" alt="">
+                    <p class="text-xl lg:text-2xl font-semibold mt-7">Daily Market Intelligence</p>
+                    <p class="text-lg mt-4">EQ Analysis is a daily, one-page strategy publication giving traders a 
+                        unique “map” through which to navigate the markets for the product they are trading.
+                    </p>
+                </div>
+                <div class="flex flex-col items-center text-center">
+                    <img src="https://astro-agency08.vercel.app/assets/img/icons/icon-tangerine-feature-2.svg"
+                        class="h-18 hue-rotate-60 invert" alt="">
+                    <p class="text-xl lg:text-2xl font-semibold mt-7">Market Intelligence for Traders</p>
+                    <p class="text-lg mt-4">EQ Analysis provides traders with a level of market intelligence about how 
+                        to “listen” to what the price action is implying about future direction each day.
+                    </p>
+                </div>
+                <div class="flex flex-col items-center text-center">
+                    <img src="https://astro-agency08.vercel.app/assets/img/icons/icon-tangerine-feature-3.svg"
+                        class="h-18 hue-rotate-60 invert" alt="">
+                    <p class="text-xl lg:text-2xl font-semibold mt-7">Enhancing Trading Strategy</p>
+                    <p class="text-lg mt-4">The 2-minute daily read will condition traders to think more about what macro range
+                        to expect for each session relative to 1-2 day EQuilibrium and how to adjust this expectation intraday, 
+                        allowing for more effective execution of their particular style of trading.</p>
+                </div>
+            </div>
+        </div>
+        <div class="mx-auto max-w-[85rem] px-4 sm:px-6 lg:px-8 py-5 sm:py-8 absolute top-0">
+            <div class="py-6"></div>
+            <h2 class="text-center w-full text-3xl lg:text-4xl 2xl:text-5xl font-semibold">IMPROVE YOUR MARKET INTELLIGENCE <br class="hidden lg:block"> WITH 
+                EQ ANALYSIS</h2>
             <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-16">
                 <div class="flex flex-col items-center text-center">
                     <img src="https://astro-agency08.vercel.app/assets/img/icons/icon-tangerine-feature-1.svg"
@@ -38,7 +69,7 @@
 
 <style>
 .background-image-cta-icon{
-    background-image: url(registration-pg-picture.jpg);
+    background-image: url(/registration-pg-picture.jpg);
     background-repeat: no-repeat;
     background-size: cover;
 }
